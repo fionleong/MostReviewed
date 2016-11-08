853069bb2d5b1da060636ceb996edbbf0929690c
naghmeh <naghmeh.anvari@sjsu.edu>
2016-11-07 18:06:54 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/naghmeh/mostreviewed/SearchController.java
@@ -0,0 +1,13 @@
+package com.example.naghmeh.mostreviewed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
---
+++ app/src/main/java/com/example/naghmeh/mostreviewed/homeController.java
@@ -0,0 +1,22 @@
+package com.example.naghmeh.mostreviewed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class homeController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void search(View view) {
+
+    }
+
+    public void surpriseMe(View view) {
+
+    }
+}
